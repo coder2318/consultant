a2462ad58df8fe1b735816b33a4fb413c7598fca
coder2318 <choriyevfarrux997@gmail.com>
2022-11-17 10:35:58 +0500

    change fileupload trait

--- app/Traits/FilesUpload.php
+++ app/Traits/FilesUpload.php
@@ -43,8 +43,8 @@ trait FilesUpload
         if (isset($params['files']) && count($params['files'])) {
             if ($model) {
                 if ($model->files) {
-//                    $i = explode(',', $model->files);
-                    foreach ($model->files as $item) {
+                    $i = explode(',', $model->getOriginal('files'));
+                    foreach ($i as $item) {
                         unlink($item);
                     }
                 }
