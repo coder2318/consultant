ef474cda79eb7f3add16df3ba7110888929e7687
coder2318 <choriyevfarrux997@gmail.com>
2022-12-01 17:31:15 +0500

    changes success

--- app/Events/StartVideoChat.php
+++ app/Events/StartVideoChat.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class StartVideoChat implements ShouldBroadcast
+class StartVideoChat implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
